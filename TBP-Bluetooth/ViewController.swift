9ef2dcc44f6b332e20f8964e66059ddb5b89f625
Cesar Rojas <caesar.red@gmail.com>
2023-06-15 18:29:07 -0500

    TBP Bluetooth start

---
+++ TBP-Bluetooth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TBP-Bluetooth
+//
+//  Created by Cesar Rojas on 6/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
